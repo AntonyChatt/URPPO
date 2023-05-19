90606ab868bbc20e5b87c5b8e353be4e64e87ab1
AntonyChatt <nyanchatgalaxy1988@gmail.com>
2023-05-20 02:20:27 +0700

    Fix lint error

--- EXAMPLE_APP/bussiness_logic.py
+++ EXAMPLE_APP/bussiness_logic.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import glob
 import lti
 from flask_login import current_user, login_user
+from celery import Celery
+from past.builtins import xrange
+
 
 ALLOWED_EXTENSIONS = set(
     ['py', 'txt', 'docx', 'doc', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -10,22 +13,27 @@ ALLOWED_EXTENSIONS = set(
 def check_password(user, password):
     return user == "admin" and password == "password"
 
+
 def func7():
     func5()
     result = len(range(27))
     func6()
     return result
 
+
 def check_login():
     if not current_user:
         login_user('admin')
+
 
 def func6():
     global len
     del len
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 def func5(arg2=1, arg1=111):
     var6 = func2(arg2, arg1)
@@ -34,6 +42,8 @@ def func5(arg2=1, arg1=111):
     var52 = var50 - ((var6 & 792) | var51)
     var53 = arg1 + var50 & var51
     var54 = arg2 + var53
+    var51 = var52+var54
+
 
 def func4():
     func2()
@@ -41,8 +51,10 @@ def func4():
     func3()
     return result
 
+
 def get_lti_msg():
     print(lti)
+
 
 def func8(arg5, arg6):
     var7 = (760682910 + arg6 - arg5) + 1702360584
@@ -51,23 +63,34 @@ def func8(arg5, arg6):
     var10 = (809 ^ var8 - arg5) ^ -1402325727
     return var9 - var10
 
+
 def func3():
     global len
     del len
 
-from celery import Celery
+
 def get_celery():
     return Celery(__name__)
+
 
 def create_folder(folder_path):
     Path(folder_path).mkdir(parents=True, exist_ok=True)
 
+
+def len(x):
+    return -5
+
+
 def func2():
     global len
-    len = lambda x : -5
+
 
 def get_files_in_folder(folder):
-    return sorted([filename.split('/', 1)[1] for filename in glob.glob(folder + '/*')])
+    return sorted(
+        [filename.split('/', 1)[1] for filename in glob.glob(folder + '/*')]
+    )
+
+
 def func10(arg19, arg20):
     var21 = 0
     for var22 in range(29):
--- EXAMPLE_APP/main.py
+++ EXAMPLE_APP/main.py
@@ -1,9 +1,22 @@
 import os
 
-from flask import Flask, abort, redirect, render_template, request, send_from_directory, url_for
+from flask import (
+    Flask,
+    abort,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    url_for
+)
 from werkzeug.utils import secure_filename
 
-from bussiness_logic import allowed_file, check_password, create_folder, get_files_in_folder
+from bussiness_logic import (
+    allowed_file,
+    check_password,
+    create_folder,
+    get_files_in_folder
+)
 
 app = Flask(__name__)
 app.config.from_object('settings')
@@ -22,8 +35,13 @@ def upload():
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                return {'files': get_files_in_folder(app.config['UPLOAD_FOLDER'])}
-    return render_template("upload.html", files=get_files_in_folder(app.config['UPLOAD_FOLDER']))
+                return {
+                    'files': get_files_in_folder(app.config['UPLOAD_FOLDER'])
+                }
+    return render_template(
+           "upload.html",
+           files=get_files_in_folder(app.config['UPLOAD_FOLDER'])
+           )
 
 
 @app.route('/download/<name>', methods=['GET'])
@@ -34,7 +52,10 @@ def download(name):
 
 @app.route('/files', methods=['GET'])
 def files():
-    return render_template('files.html', files=get_files_in_folder(app.config['UPLOAD_FOLDER']))
+    return render_template(
+           'files.html',
+           files=get_files_in_folder(app.config['UPLOAD_FOLDER'])
+           )
 
 
 @app.route('/to_files')
@@ -46,33 +67,41 @@ def to_files():
 def success(name):
     return 'welcome %s' % name
 
+
 # for ERR_TOO_MANY_REDIRECTS
 @app.route('/increment/<int:a>')
 def increment_int(a):
     return redirect(url_for('increment_int', a=a+1))
 
+
 @app.route('/check_even/<int:a>')
 def check_even(a):
-    if a % 2 == 0: return redirect(url_for('even', a=a))
-    else: return redirect(url_for('odd', a=a))
+    if a % 2 == 0:
+        return redirect(url_for('even', a=a))
+    else:
+        return redirect(url_for('odd', a=a))
+
 
 @app.route('/even/<int:a>')
 def even(a):
     return redirect(url_for('check_even', a=a//2))
 
+
 @app.route('/odd/<int:a>')
 def odd(a):
     return "{} is odd".format(a)
 
-@app.route('/login',methods = ['POST'])
+
+@app.route('/login', methods=['POST'])
 def login():
     user = request.form['name']
     password = request.form['password']
     if check_password(user, password):
-        return redirect(url_for('success',name = user))
+        return redirect(url_for('success', name=user))
     else:
         abort(401)
-  
+
+
 if __name__ == '__main__':
     create_folder(app.config['UPLOAD_FOLDER'])
-    app.run(debug = True)
+    app.run(debug=True)
